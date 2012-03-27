33e213ed7b7ad9c0b26925377ec887cb43d3351c
Justin Sinclair <jsinclair@tracmor.com>
2012-03-26 17:06:21 -0700

    Include custom fields in asset transaction datagrid, cont'd again.

--- includes/qcodo/qform/QAssetTransactComposite.class.php
+++ includes/qcodo/qform/QAssetTransactComposite.class.php
@@ -979,7 +979,7 @@ class QAssetTransactComposite extends QControl {
 		// Redeclare in case the asset has been edited
 		$this->objAssetArray = null;
 		if ($this->blnEditMode && $this->objAsset instanceof Asset) {
-			$this->objAssetArray[] = Asset::Load($this->objAsset->AssetId);
+			$this->objAssetArray[] = Asset::LoadByAssetCodeWithCustomFields($this->objAsset->AssetCode);
 			// Load all child assets
 			$objLinkedAssetArray = Asset::LoadChildLinkedArrayByParentAssetId($this->objAsset->AssetId);
 			if ($objLinkedAssetArray) {
