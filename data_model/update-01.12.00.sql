bf9ee244d14c6ba359df308df4410d81d0cb8ba9
Justin Sinclair <jsinclair@tracmor.com>
2012-11-07 10:24:50 -0800

    Added missing _core images and removed old SQL scripts

--- data_model/update-01.12.00.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE  `location` ADD  `inventory_flag` BIT( 1 ) NOT NULL DEFAULT b'1' AFTER  `long_description`;
-ALTER TABLE  `location` ADD  `asset_flag`     BIT( 1 ) NOT NULL DEFAULT b'1' AFTER  `long_description`;
